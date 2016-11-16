184223079abdd68bfb80d4801ed725cbb657e4b1
Gia Huy <giahuy7414@gmail.com>
2016-11-16 21:11:23 +0700

    remove overide SupplyOrder.php AdminPdfController.php

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -2,6 +2,4 @@
 
 	require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.'HTMLTemplateSupplyOrderVoucher.php');
     require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.'CapPDF.php');
-    require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'SupplyOrderVoucher.php');
-    require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'SupplyOrder.php');
-    require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.'CapAdminPdfController.php');
+    require_once  (_PS_MODULE_DIR_.'supplyordervoucherpdf'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'stock'.DIRECTORY_SEPARATOR.'SupplyOrderVoucher.php');
